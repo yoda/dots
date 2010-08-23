50749c2df1b252225c69b6c967df9b3627b16fda
yoda <yoda@tallon4.net>
2010-08-24 02:20:03 +0800

    Made it work, now needs recovery

--- setup.sh
+++ setup.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
+export ORIGINAL_PATH="$(pwd)"
+cd "$(dirname $0)"
+# Absolute Path to the bin Dir
+export BIN_PATH="$(pwd)"
+cd "${ORIGINAL_PATH}"
+export APPLICATION_PATH="$(dirname ${BIN_PATH})"
+echo $APPLICATION_PATH
+export ORIGINAL_PATH="$(pwd)"
+echo $ORIGINAL_PATH
+SCRIPTDIR=$ORIGINAL_PATH
+echo $SCRIPTDIR
 
 # Setup screen
-ln -s screen/screenrc "$HOME/.screenrc" 
+ln -s $SCRIPTDIR/screen/screenrc "$HOME/.screenrc" 
 
 # Setup tmux
-ln -s tmux/screenrc "$HOME/.tmux.conf"
+ln -s $SCRIPTDIR/tmux/screenrc "$HOME/.tmux.conf"
 
 # Setup vim
-ln -s vim/vimrc "$HOME/.vimrc"
-ln -s vim/ "$HOME/.vim"
+ln -s $SCRIPTDIR/vim/vimrc "$HOME/.vimrc"
+ln -s $SCRIPTDIR/vim/ "$HOME/.vim"
 
 # Setup awesome
-ln -s awesome/config/ "$HOME/.config"
+ln -s $SCRIPTDIR/awesome/config/ "$HOME/.config"
 
 # Setup Xorg
-ln -s Xorg/Xresources "$HOME/.Xdefaults"
-ln -s Xorg/Xinit "$HOME/.xinitrc"
+ln -s $SCRIPTDIR/Xorg/Xresources "$HOME/.Xdefaults"
+ln -s $SCRIPTDIR/Xorg/Xinit "$HOME/.xinitrc"
 
 # Setup irssi
-ln -s irssi/irssi/ "$HOME/.irssi"
+ln -s $SCRIPTDIR/irssi/irssi/ "$HOME/.irssi"
 
 
