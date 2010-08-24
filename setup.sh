8559162ead9fd462e55c5addf07e3c245caf1dbb
yoda <yoda@tallon4.net>
2010-08-24 13:06:23 +0800

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -5,30 +5,93 @@ cd "$(dirname $0)"
 export BIN_PATH="$(pwd)"
 cd "${ORIGINAL_PATH}"
 export APPLICATION_PATH="$(dirname ${BIN_PATH})"
-echo $APPLICATION_PATH
 export ORIGINAL_PATH="$(pwd)"
-echo $ORIGINAL_PATH
 SCRIPTDIR=$ORIGINAL_PATH
 echo $SCRIPTDIR
 
-# Setup screen
-ln -s $SCRIPTDIR/screen/screenrc "$HOME/.screenrc" 
 
-# Setup tmux
-ln -s $SCRIPTDIR/tmux/screenrc "$HOME/.tmux.conf"
+export SCREENCONF="$HOME/.screenrc" 
+export TMUXCONF="$HOME/.tmux.conf"
+export VIMRCCONF="$HOME/.vimrc"
+export VIMCONF="$HOME/.vim"
+export AWESOMECONF="$HOME/.config"
+export XRESCONF="$HOME/.Xdefaults"
+export XINITRCCONF="$HOME/.xinitrc"
+export IRSSICONF="$HOME/.irssi"
 
-# Setup vim
-ln -s $SCRIPTDIR/vim/vimrc "$HOME/.vimrc"
-ln -s $SCRIPTDIR/vim/ "$HOME/.vim"
-
-# Setup awesome
-ln -s $SCRIPTDIR/awesome/config/ "$HOME/.config"
-
-# Setup Xorg
-ln -s $SCRIPTDIR/Xorg/Xresources "$HOME/.Xdefaults"
-ln -s $SCRIPTDIR/Xorg/Xinit "$HOME/.xinitrc"
-
-# Setup irssi
-ln -s $SCRIPTDIR/irssi/irssi/ "$HOME/.irssi"
+export ERRORMSG="exists backup or remove"
+export COMPLETEMSG="Created link successfully"
 
 
+if [ -e $SCREENCONF ]
+then
+    echo $SCREENCONF $ERRORMSG 
+else
+    echo $SCREENCONF $COMPLETEMSG
+    # Setup screen
+    ln -s $SCRIPTDIR/screen/screenrc $SCREENCONF
+fi
+
+if [ -e $TMUXCONF ]
+then
+    echo $TMUXCONF $ERRORMSG
+else
+    echo $TMUXCONF $COMPLETEMSG
+    # Setup tmux
+    ln -s $SCRIPTDIR/tmux/screenrc $TMUXCONF 
+fi
+
+if [ -e $VIMRCCONF ]
+then
+    echo $VIMRCCONF $ERRORMSG
+else
+    echo $VIMRCCONF $COMPLETEMSG
+    # Setup vim
+    ln -s $SCRIPTDIR/vim/vimrc $VIMRCCONF
+fi
+
+
+if [ -e $VIMCONF ]
+then
+    echo $VIMCONF $ERRORMSG
+else
+    echo $VIMCONF $COMPLETEMSG
+    # Setup vim 
+    ln -s $SCRIPTDIR/vim/ $VIMCONF 
+fi
+
+if [ -e $AWESOMECONF ]
+then
+    echo $AWESOMECONF $ERRORMSG
+else
+    echo $AWESOMECONF $COMPLETEMSG
+    # Setup awesome
+    ln -s $SCRIPTDIR/awesome/config/ $AWESOMECONF
+fi
+
+if [ -e $XRESCONF ]
+then
+    echo $XRESCONF $ERRORMSG
+else
+    echo $XRESCONF $COMPLETEMSG
+    # Setup Xorg
+    ln -s $SCRIPTDIR/Xorg/Xresources $XRESCONF
+fi
+
+if [ -e $XINITRCCONF ]
+then
+    echo $XINITRCCONF $ERRORMSG
+else
+    echo $XINITRCCONF $COMPLETEMSG
+    # Setup xinit
+    ln -s $SCRIPTDIR/Xorg/Xinit $XINITRCCONF
+fi
+
+if [ -e $IRSSICONF ]
+then
+    echo $IRSSICONF $ERRORMSG
+else
+    echo $IRSSICONF $COMPLETEMSG
+    # Setup irssi
+    ln -s $SCRIPTDIR/irssi/irssi/ $IRSSICONF 
+fi
